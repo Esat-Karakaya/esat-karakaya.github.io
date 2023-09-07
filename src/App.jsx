31af267e413a36243c9fa989fa71ac672ee2482c
Murat-Karakaya <muratkrky09@gmail.com>
2023-09-07 17:48:52 +0300

    animation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import Nav from "./components/nav/nav";
 import PageContainer from "./components/pageContainer/pageContainer";
 import ContactForm from "./components/modal/modal"
+import { useEffect } from "react";
 
 const App=()=> {
+    useEffect(()=>{
+        const observer =new IntersectionObserver((entries)=>{
+            entries.forEach((entry)=>{
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show")
+                    observer.disconnect();
+                }
+            })
+        })
+        const hiddenElements = document.querySelectorAll(".reveal")
+        hiddenElements.forEach((el) => observer.observe(el))
+    },[])
     return(
         <>
             <Nav/>
--- src/components/aboutPage/About.jsx
+++ src/components/aboutPage/About.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react"
 import ContentImg from "../contentImg/contentImg"
 import Page from "../page/page"
 import "./aboutPageStyle.css"
@@ -6,20 +5,6 @@ const About=()=>{
     const startViewing=()=>{
         document.querySelector("#startWith").scrollIntoView({behavior:"smooth"})
     }
-
-    useEffect(()=>{
-        const observer =new IntersectionObserver((entries)=>{
-            entries.forEach((entry)=>{
-                if (entry.isIntersecting) {
-                    entry.target.classList.add("show")
-                } else {
-                    entry.target.classList.add("hide")
-                }
-            })
-        })
-        const hiddenElements = document.querySelectorAll(".hidden")
-        hiddenElements.forEach((el) => observer.observe(el))
-    },[])
     return(
         <Page id="0">
             <section>
@@ -28,16 +13,19 @@ const About=()=>{
                     <ContentImg alt="me" imgurl="/me.webp"/>
                     <p>I'm Esat, a 14 year old who has a passion for programming. I'm a self-thought full stack programmer and can both visualize and handle data. But there is much more to that! Which is why i separated them to two different pages. Here i just want to introduce myself 😉</p>
                 </div>
-                <button onClick={startViewing} className="pageButton">Let's Start</button>
+                <button onClick={startViewing} className="startButton">Let's Start</button>
             </section>
             <section id="startWith">
                 <p>If you want to connect with me and check out my work, you can find me on GitHub, LinkedIn and Twitter. Feel free to drop by and say hi! 😎</p>
                 <div className="linkLineup"
-                 style={{width:"100%", justifyContent:"center", rowGap:"1rem",columnGap:"5rem"}}>
-                    <a href="#"><img className="profileLink" alt="Github logo" src="/github.svg"/></a>
-                    <a href="#"><img className="profileLink" alt="LinkedIn logo" src="/linkedin.png"/></a>
-                    <a href="#"><img className="profileLink" alt="Twitter logo" src="/twitter.svg"/></a>
+                 style={{width:"100%", justifyContent:"center", columnGap:"10%"}}>
+                    <a href="#"><img style={{"--order":"0"}} className="reveal logo" alt="Github logo" src="/github.svg"/></a>
+                    <a href="#"><img style={{"--order":"1"}} className="reveal logo" alt="LinkedIn logo" src="/linkedin.png"/></a>
+                    <a href="#"><img style={{"--order":"2"}} className="reveal logo" alt="Twitter logo" src="/twitter.svg"/></a>
                 </div>
+            </section>
+            <section>
+                <p>When I'm not coding or diving into math problems, I enjoy learning German and English. I'm also a nature enthusiast, so you'll often find me swimming, climbing, or hiking 😊</p>
             </section>
         </Page>
     )
